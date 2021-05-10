ebdaa0b8a409a1f0a9032ba0360328bcf0417ac3
Emin Šiljak <56275045+esiljak1@users.noreply.github.com>
2021-05-10 22:19:01 +0200

    Added quadrants to sudoku board

--- Backtracking algorithm/Program.cs
+++ Backtracking algorithm/Program.cs
@@ -3,7 +3,18 @@
 namespace Backtracking_algorithm {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int[,] board = new int[,] {
+                {3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                {5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                {0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                {9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                {0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                {1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                {0, 0, 5, 2, 0, 6, 3, 0, 0 }
+            };
+            Sudoku sudoku = new Sudoku(board);
         }
     }
 }
--- Backtracking algorithm/Sudoku.cs
+++ Backtracking algorithm/Sudoku.cs
@@ -4,16 +4,17 @@ using System.Text;
 
 namespace Backtracking_algorithm {
     class Sudoku {
-        private int[][] board = new int[9][];
+        private static readonly int NUM_ROWS = 9, NUM_COLS = 9;
 
-        private bool checkBoard(int[][] b) {
-            if (b.Length != 9)
+        private int[,] board = new int[9,9];
+        private int[,] quadrants = new int[9,9];
+
+        private bool checkBoard(int[,] b) {
+            if (b.GetLength(0) != 9 || b.GetLength(1) != 9)
                 return false;
-            for(int i = 0; i < b.Length; i++) {
-                if (b[i].Length != 9)
-                    return false;
-                for(int j = 0; j < b[i].Length; j++) {
-                    if (b[i][j] < 0 || b[i][j] > 9)
+            for(int i = 0; i < b.GetLength(1); i++) {
+                for(int j = 0; j < b.GetLength(0); j++) {
+                    if (b[i,j] < 0 || b[i,j] > 9)
                         return false;
                 }
             }
@@ -21,25 +22,96 @@ namespace Backtracking_algorithm {
             return true;
         }
 
-        public Sudoku(int[][] board) {
+        private bool checkField(int num, int row, int col) {
+            for (int i = 0; i < 9; i++) {
+                if (i != col && board[row,i] == num)
+                    return false;
+            }
+            return true;
+        }
+
+        private void setQuadrants(int[,] board) {
+            int qNum = 0, col = -3;
+            for(int i = 0; i < 9; i++) {
+                col += 3;
+                if (i != 0 && i % 3 == 0) {
+                    qNum += 3;
+                    col = 0;
+                }
+                    
+                for(int j = 0; j < 9; j++) {
+                    if (j != 0 && j % 3 == 0)
+                        qNum++;
+                    else if (qNum != 0 && j == 0)
+                        qNum -= 2;
+
+                    quadrants[qNum,col++] = board[i,j];
+                    if (col != 0 && col % 3 == 0)
+                        col -= 3;
+                }
+            }
+        }
+
+        private bool checkQuadrant(int num, int row, int col) {
+            int qNum = 0;
+            if (col >= 3 && col <= 5)
+                qNum = 1;
+            else if (col >= 6 && col <= 8)
+                qNum = 2;
+
+            if (row >= 3 && row >= 5)
+                qNum++;
+            else if (row >= 6 && row <= 8)
+                qNum += 2;
+
+            for(int j = 0; j < 9; j++) {
+                if (quadrants[qNum,j] == num)
+                    return false;
+            }
+            return true;
+        }
+
+        private void printBoard() {
+            for(int i = 0; i < NUM_ROWS; i++) {
+                for(int j = 0; j < NUM_COLS; j++) {
+                    Console.WriteLine(board[i, j]);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        public Sudoku(int[,] board) {
             if (!checkBoard(board)) {
                 throw new ArgumentException("Board must be 9x9 and use only numbers between 0 and 9, where 0 represents blank space");
             }
             this.board = board;
+            setQuadrants(board);
+            printQuadrants();
+        }
+
+        public void printQuadrants() {
+            for(int i = 0; i < quadrants.GetLength(1); i++) {
+                for(int j = 0; j < NUM_ROWS; j++) {
+                    Console.WriteLine(quadrants[i,j] + " ");
+                }
+                Console.WriteLine("\n");
+            }
         }
 
         public bool changeField(int num, int row, int col) {
             if (num < 0 || num > 9 || row < 0 || row > 8 || col < 0 || col > 8)
                 throw new ArgumentException("Number or row or column is not within the range");
-            for(int i = 0; i < board.Length; i++) {
-                if (i != col && board[row][i] == num)
-                    return false;
-            }
-            for(int i = 0; i < board[row].Length; i++) {
-                if (i != row && board[i][col] == num)
-                    return false;
-            }
-            board[row][col] = num;
+            //check if there's already that number in a given row
+            if (!checkField(num, row, col))
+                return false;
+            //check if there's already that number in a given column
+            if(!checkField(num, col, row))
+                return false;
+            //check if there's already that number in a given quadrant
+            if (!checkQuadrant(num, row, col))
+                return false;
+
+            board[row,col] = num;
             return true;
         }
     }
