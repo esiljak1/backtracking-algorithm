56e67b5f32834771bd0c845bfdf831c19a3f8f07
Emin Šiljak <56275045+esiljak1@users.noreply.github.com>
2021-05-10 23:20:14 +0200

    Now using list of lists from collection class and initialBoard is remembered

--- Backtracking algorithm/Program.cs
+++ Backtracking algorithm/Program.cs
@@ -1,20 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Backtracking_algorithm {
     class Program {
         static void Main(string[] args) {
-            int[,] board = new int[,] {
-                {3, 0, 6, 5, 0, 8, 4, 0, 0 },
-                {5, 2, 0, 0, 0, 0, 0, 0, 0 },
-                {0, 8, 7, 0, 0, 0, 0, 3, 1 },
-                {0, 0, 3, 0, 1, 0, 0, 8, 0 },
-                {9, 0, 0, 8, 6, 3, 0, 0, 5 },
-                {0, 5, 0, 0, 9, 0, 6, 0, 0 },
-                {1, 3, 0, 0, 0, 0, 2, 5, 0 },
-                {0, 0, 0, 0, 0, 0, 0, 7, 4 },
-                {0, 0, 5, 2, 0, 6, 3, 0, 0 }
+            List<List<int>> board = new List<List<int>> {
+                new List<int>{3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                new List<int>{5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                new List<int>{0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                new List<int>{0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                new List<int>{9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                new List<int>{0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                new List<int>{1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                new List<int>{0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                new List<int>{0, 0, 5, 2, 0, 6, 3, 0, 0 }
             };
             Sudoku sudoku = new Sudoku(board);
+            if(sudoku.changeField(4, 8, 0)) {
+                sudoku.printInitialBoard();
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                sudoku.printPlayingBoard();
+            } else {
+                Console.WriteLine("NOT OK");
+            }
         }
     }
 }
--- Backtracking algorithm/Sudoku.cs
+++ Backtracking algorithm/Sudoku.cs
@@ -6,15 +6,19 @@ namespace Backtracking_algorithm {
     class Sudoku {
         private static readonly int NUM_ROWS = 9, NUM_COLS = 9;
 
-        private int[,] board = new int[9,9];
-        private int[,] quadrants = new int[9,9];
+        private List<List<int>> initialBoard = new List<List<int>>();
+        private List<List<int>> quadrants = new List<List<int>>();
 
-        private bool checkBoard(int[,] b) {
-            if (b.GetLength(0) != 9 || b.GetLength(1) != 9)
+        private List<List<int>> playingBoard = new List<List<int>>();
+
+        private bool checkBoard(List<List<int>> b) {
+            if (b.Count != 9)
                 return false;
-            for(int i = 0; i < b.GetLength(1); i++) {
-                for(int j = 0; j < b.GetLength(0); j++) {
-                    if (b[i,j] < 0 || b[i,j] > 9)
+            for(int i = 0; i < b.Count; i++) {
+                if (b[i].Count != 9)
+                    return false;
+                for(int j = 0; j < b[i].Count; j++) {
+                    if (b[i][j] < 0 || b[i][j] > 9)
                         return false;
                 }
             }
@@ -22,15 +26,16 @@ namespace Backtracking_algorithm {
             return true;
         }
 
-        private bool checkField(int num, int row, int col) {
+        private bool checkColumn(int num, int col) {
             for (int i = 0; i < 9; i++) {
-                if (i != col && board[row,i] == num)
+                if (playingBoard[i][col] == num)
                     return false;
             }
             return true;
         }
 
-        private void setQuadrants(int[,] board) {
+        private void setQuadrants(List<List<int>> board) {
+            quadrants = board.ConvertAll(list => new List<int>(list));
             int qNum = 0, col = -3;
             for(int i = 0; i < 9; i++) {
                 col += 3;
@@ -45,7 +50,7 @@ namespace Backtracking_algorithm {
                     else if (qNum != 0 && j == 0)
                         qNum -= 2;
 
-                    quadrants[qNum,col++] = board[i,j];
+                    quadrants[qNum][col++] = board[i][j];
                     if (col != 0 && col % 3 == 0)
                         col -= 3;
                 }
@@ -65,34 +70,45 @@ namespace Backtracking_algorithm {
                 qNum += 2;
 
             for(int j = 0; j < 9; j++) {
-                if (quadrants[qNum,j] == num)
+                if (quadrants[qNum][j] == num)
                     return false;
             }
             return true;
         }
 
-        private void printBoard() {
-            for(int i = 0; i < NUM_ROWS; i++) {
-                for(int j = 0; j < NUM_COLS; j++) {
-                    Console.WriteLine(board[i, j]);
+        public void printInitialBoard() {
+            for (int i = 0; i < NUM_ROWS; i++) {
+                for (int j = 0; j < NUM_COLS; j++) {
+                    Console.Write(initialBoard[i][j] + " ");
                 }
-                Console.WriteLine("\n");
+                Console.WriteLine();
             }
         }
 
-        public Sudoku(int[,] board) {
+        public void printPlayingBoard() {
+            for (int i = 0; i < NUM_ROWS; i++) {
+                for(int j = 0; j < NUM_COLS; j++) {
+                    Console.Write(playingBoard[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public Sudoku(List<List<int>> board) {
             if (!checkBoard(board)) {
                 throw new ArgumentException("Board must be 9x9 and use only numbers between 0 and 9, where 0 represents blank space");
             }
-            this.board = board;
+
+            initialBoard = board.ConvertAll(list => new List<int>(list));
+            playingBoard = board.ConvertAll(list => new List<int>(list));
+
             setQuadrants(board);
-            printQuadrants();
         }
 
-        public void printQuadrants() {
-            for(int i = 0; i < quadrants.GetLength(1); i++) {
+        private void printQuadrants() {
+            for(int i = 0; i < quadrants.Count; i++) {
                 for(int j = 0; j < NUM_ROWS; j++) {
-                    Console.WriteLine(quadrants[i,j] + " ");
+                    Console.WriteLine(quadrants[i][j] + " ");
                 }
                 Console.WriteLine("\n");
             }
@@ -101,17 +117,25 @@ namespace Backtracking_algorithm {
         public bool changeField(int num, int row, int col) {
             if (num < 0 || num > 9 || row < 0 || row > 8 || col < 0 || col > 8)
                 throw new ArgumentException("Number or row or column is not within the range");
+            //check if cell can be changed
+            if (initialBoard[row][col] != 0)
+                throw new ArithmeticException("You cannot change this cell");
+            if(num == 0) {
+                playingBoard[row][col] = num;
+                return true;
+            }
+
             //check if there's already that number in a given row
-            if (!checkField(num, row, col))
+            if (playingBoard[row].Contains(num))
                 return false;
             //check if there's already that number in a given column
-            if(!checkField(num, col, row))
+            if(!checkColumn(num, col))
                 return false;
             //check if there's already that number in a given quadrant
             if (!checkQuadrant(num, row, col))
                 return false;
 
-            board[row,col] = num;
+            playingBoard[row][col] = num;
             return true;
         }
     }
